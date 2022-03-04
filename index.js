31bed5ff80fef93ccc3efb984312f5699d5e7a51
mwlk <mirkoivowlk@gmail.com>
2022-03-04 00:02:12 -0300

    upd index

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-const {Client } = require('whatsapp-web.js');
-const client = new Client();
+const fs = require("fs");
+const ora = require("ora");
+const chalk = require("chalk");
 
-client.on('qr', (qr) => {
-    console.log('qr recibido', qr)
-});
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
-client.on('ready', () => {
-    console.log('ready')
-});
+const SESSION_FILE_PATH = "./session.json";
+let client;
+let sessionData;
+
+const withSession = () => {
+  const spinner = ora(`Loading ${chalk.yellow("validando")}`);
+  sessionData = require(SESSION_FILE_PATH);
+  spinner.start();
+  client = new Client({
+    session: sessionData,
+  });
+
+  client.on("ready", () => {
+    console.log("ready");
+    spinner.stop();
+  });
+
+  client.on("auth_failure", () => {
+    spinner.stop();
+    console.log("error in auth");
+  });
+
+  client.initialize();
+};
+
+const withOutSession = () => {
+  console.log("sin sesion");
+  client = new Client();
+  client.on("qr", (qr) => {
+    qrcode.generate(qr), { small: true };
+  });
+
+  client.on("authenticated", (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  });
+
+  client.initialize();
+};
+
+(fs.existsSync(SESSION_FILE_PATH)) ? withSession() : withOutSession();
